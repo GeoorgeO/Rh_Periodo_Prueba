2d0b1b42d7c599039d9d4dbebbba06ada7da8d86
George O <geoorge191@hotmail.com>
2019-10-07 09:43:44 -0500

    Cambio a 30 y 90 dias naturales

---
+++ Code/rh_periodo_prueba/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(58, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(340, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(5, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Fecha de inicio:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(98, 38)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(98, 20)
+        Me.DateTimePicker1.TabIndex = 3
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.SteelBlue
+        Me.ListView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListView1.Location = New System.Drawing.Point(5, 131)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(464, 197)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.CadetBlue
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(219, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 23)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Agregar"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Tomato
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(313, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 23)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Eliminar"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Location = New System.Drawing.Point(4, 99)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(394, 26)
+        Me.Panel1.TabIndex = 7
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.ForeColor = System.Drawing.Color.Black
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", "Todos"})
+        Me.ComboBox1.Location = New System.Drawing.Point(249, 0)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 13
+        Me.ComboBox1.Text = "Buscar por mes"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(128, 4)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(16, 16)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "X"
+        Me.Label5.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.GrayText
+        Me.TextBox2.Location = New System.Drawing.Point(15, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox2.TabIndex = 11
+        Me.TextBox2.Text = "Buscar por nombre"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(205, 42)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Periodo de dias:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"30", "90"})
+        Me.ComboBox2.Location = New System.Drawing.Point(288, 38)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(110, 21)
+        Me.ComboBox2.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SteelBlue
+        Me.ClientSize = New System.Drawing.Size(472, 335)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Agregar nuevos empleados"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ Code/rh_periodo_prueba/Form1.vb
@@ -0,0 +1,386 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim valor As String = ""
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        recargar()
+    End Sub
+
+    Function calis(ByVal nombre As String, ByVal fecha As Date)
+        Dim dia As Integer = 0
+        Dim mes As Integer = 0
+        Dim anio As Integer = 0
+        Dim fechaC As Date
+
+        dia = CDate(fecha).Date.Day
+        mes = CDate(fecha).Date.Month
+        anio = CDate(fecha).Date.Year
+        If mes <= 9 Then
+            fechaC = dia & "/" & mes + 3 & "/" & anio
+        Else
+            fechaC = dia & "/" & 3 - (12 - mes) & "/" & anio + 1
+        End If
+
+        MessageBox.Show(fechaC, "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+
+        fechaC = fecha.AddMonths(+3)
+        MessageBox.Show(fechaC, "My Application", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk)
+
+        Return Nothing
+    End Function
+
+    Function agregaTabla(ByVal nombre As String, ByVal fecha As Date)
+        'Dim dia As Integer = 0
+        'Dim mes As Integer = 0
+        'Dim anio As Integer = 0
+        Dim fechaC As Date
+        Dim ndays As Int32 = 0
+
+        'dia = CDate(fecha).Date.Day
+        'mes = CDate(fecha).Date.Month
+        'anio = CDate(fecha).Date.Year
+        'If mes <= 9 Then
+        '    fechaC = dia & "/" & mes + 3 & "/" & anio
+        'Else
+        '    fechaC = dia & "/" & 3 - (12 - mes) & "/" & anio + 1
+        'End If
+        ndays = CInt(ComboBox2.GetItemText(ComboBox2.SelectedItem))
+
+        fechaC = fecha.AddDays(ndays)
+        'fechaC = fecha.AddMonths(+3)
+
+        Try
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    cmd.CommandText = "SET DATEFORMAT dmy   insert into RH_periodoPrueba (Nombre,fechaInicio,fechaFin,NumDays) values (@nombre,@fecha,@fCumple,@ndays)"
+                    cmd.Parameters.AddWithValue("@nombre", nombre)
+                    cmd.Parameters.AddWithValue("@fecha", fecha)
+                    cmd.Parameters.AddWithValue("@fCumple", fechaC)
+                    cmd.Parameters.AddWithValue("@ndays", ndays)
+
+
+                    cmd.ExecuteNonQuery()
+
+                    Dim Litems As New ListViewItem
+
+                    Litems = ListView1.Items.Add(nombre)
+                    Litems.SubItems.Add(fechaC)
+                    Litems.SubItems.Add(ndays)
+                End Using
+                cnx.Close()
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return Nothing
+    End Function
+
+
+    Function recargar()
+        ComboBox2.SelectedIndex = 1
+
+        ListView1.Clear() 'Limpiamos el ListView
+        ListView1.View = View.Details 'Tipo de vista
+        ListView1.FullRowSelect = True 'Al seleccionar un elemento, seleccionar la línea completa
+        ListView1.GridLines = True 'Mostrar las líneas de la cuadrícula
+        ListView1.LabelEdit = False 'No permitir la edición automática del texto
+        ListView1.MultiSelect = False 'Permitir múltiple selección
+        ListView1.HideSelection = False 'Para que al perder el foco, se siga viendo el que está seleccionado
+        ListView1.ShowGroups = False 'Listado NO Agrupado
+
+        ListView1.Columns.Add("Nombre", 270, HorizontalAlignment.Left)
+        ListView1.Columns.Add("Fin de periodo prueba", 120, HorizontalAlignment.Left)
+        ListView1.Columns.Add("Num. Dias", 70, HorizontalAlignment.Left)
+        ListView1.View = View.Details
+
+        cargarTabla()
+        Return Nothing
+    End Function
+
+
+    Function AgregaLista(ByVal nombre As String, ByVal fecha As String, ByVal ndays As Int32)
+
+        Dim Litems As New ListViewItem
+
+        Litems = ListView1.Items.Add(nombre)
+        Litems.SubItems.Add(CDate(fecha))
+        Litems.SubItems.Add(CInt(ndays))
+
+        Return Nothing
+    End Function
+
+
+    Function cargarTabla()
+        Dim nombre As String = ""
+        Dim fecha As String = ""
+        Dim numdays As Int32 = 0
+        Dim lector As SqlDataReader
+        Try
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    cmd.CommandText = "SET DATEFORMAT dmy   select Nombre, CONVERT(CHAR(10), fechaFin, 111) as fechaFin,isnull(NumDays,0) as NumDays from RH_periodoPrueba order by fechaFin"
+                    'cmd.ExecuteNonQuery()
+
+                    lector = cmd.ExecuteReader
+                    While lector.Read
+
+                        nombre = CStr(lector(0).ToString)
+                        fecha = CStr(lector(1).ToString)
+                        numdays = CInt(lector(2).ToString)
+
+
+                        AgregaLista(nombre, fecha, numdays)
+                    End While
+                    lector.Close()
+
+                End Using
+                cnx.Close()
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return Nothing
+    End Function
+
+    Private Sub Label3_Click(sender As System.Object, e As System.EventArgs) Handles Label3.Click
+        If TextBox1.Text <> "" Then
+            agregaTabla(TextBox1.Text, CDate(DateTimePicker1.Text))
+            'calis(TextBox1.Text, CDate(DateTimePicker1.Text))
+            TextBox1.Text = ""
+            DateTimePicker1.Value = Today
+        End If
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+        Try
+            valor = ListView1.SelectedItems(0).Text
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
+    Function eliminarTabla(ByVal nombre As String)
+        Try
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    cmd.CommandText = "delete from RH_periodoPrueba where Nombre='" & nombre & "'"
+                    cmd.ExecuteNonQuery()
+
+                    recargar()
+                End Using
+                cnx.Close()
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        Return Nothing
+    End Function
+
+    Private Sub Label4_Click(sender As System.Object, e As System.EventArgs) Handles Label4.Click
+        eliminarTabla(valor)
+    End Sub
+
+    Function recargarbusca()
+        ListView1.Clear() 'Limpiamos el ListView
+        ListView1.View = View.Details 'Tipo de vista
+        ListView1.FullRowSelect = True 'Al seleccionar un elemento, seleccionar la línea completa
+        ListView1.GridLines = True 'Mostrar las líneas de la cuadrícula
+        ListView1.LabelEdit = False 'No permitir la edición automática del texto
+        ListView1.MultiSelect = False 'Permitir múltiple selección
+        ListView1.HideSelection = False 'Para que al perder el foco, se siga viendo el que está seleccionado
+        ListView1.ShowGroups = False 'Listado NO Agrupado
+
+        ListView1.Columns.Add("Nombre", 270, HorizontalAlignment.Left)
+        ListView1.Columns.Add("Fin de periodo prueba", 120, HorizontalAlignment.Left)
+        ListView1.View = View.Details
+
+        cargarTablabusca()
+        Return Nothing
+    End Function
+
+    Function cargarTablabusca()
+        Dim nombre As String = ""
+        Dim fecha As String = ""
+        Dim numdays As Int32 = 0
+        Dim lector As SqlDataReader
+        Try
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    cmd.CommandText = "SET DATEFORMAT dmy   select Nombre, CONVERT(CHAR(10), fechaFin, 111) as fechaFin, isnull(NumDays,0) as NumDays from RH_periodoPrueba where Nombre LIKE '" & TextBox2.Text & "%' Order by fechaFin"
+                    'cmd.ExecuteNonQuery()
+
+                    lector = cmd.ExecuteReader
+                    While lector.Read
+
+                        nombre = CStr(lector(0).ToString)
+                        fecha = CStr(lector(1).ToString)
+                        numdays = CInt(lector(2).ToString)
+
+
+                        AgregaLista(nombre, fecha, numdays)
+                    End While
+                    lector.Close()
+
+                End Using
+                cnx.Close()
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return Nothing
+    End Function
+
+    Function recargarmes(ByVal mes As Integer, ByVal anio As Integer)
+        ListView1.Clear() 'Limpiamos el ListView
+        ListView1.View = View.Details 'Tipo de vista
+        ListView1.FullRowSelect = True 'Al seleccionar un elemento, seleccionar la línea completa
+        ListView1.GridLines = True 'Mostrar las líneas de la cuadrícula
+        ListView1.LabelEdit = False 'No permitir la edición automática del texto
+        ListView1.MultiSelect = False 'Permitir múltiple selección
+        ListView1.HideSelection = False 'Para que al perder el foco, se siga viendo el que está seleccionado
+        ListView1.ShowGroups = False 'Listado NO Agrupado
+
+        ListView1.Columns.Add("Nombre", 270, HorizontalAlignment.Left)
+        ListView1.Columns.Add("Fin de periodo prueba", 120, HorizontalAlignment.Left)
+        ListView1.View = View.Details
+
+        cargarTablames(mes, anio)
+        Return Nothing
+    End Function
+
+    Function cargarTablames(ByVal mes As Integer, ByVal anio As Integer)
+        Dim nombre As String = ""
+        Dim fecha As String = ""
+        Dim numdays As Int32 = 0
+        Dim lector As SqlDataReader
+        Try
+            Using cnx = New SqlConnection("Data Source=192.168.3.254;Initial Catalog=Vistas;User ID=sa;Password=inventumc762$")
+                cnx.Open()
+                Using cmd As New SqlCommand()
+                    cmd.Connection = cnx
+                    Dim messig, aniosig As Integer
+                    If mes < 12 Then
+                        messig = mes + 1
+                        aniosig = anio
+                    End If
+                    If mes = 12 Then
+                        messig = 1
+
+                        aniosig = anio + 1
+                    End If
+
+                    cmd.CommandText = "SET DATEFORMAT dmy   select Nombre, CONVERT(CHAR(10), fechaFin, 111) as fechaFin,isnull(NumDays,0) as NumDays from RH_periodoPrueba where fechaFin>='01-" & mes & "-" & anio & "' and fechaFin<'01-" & messig & "-" & aniosig & "' Order by fechaFin"
+                    'cmd.ExecuteNonQuery()
+
+                    lector = cmd.ExecuteReader
+                    While lector.Read
+
+                        nombre = CStr(lector(0).ToString)
+                        fecha = CStr(lector(1).ToString)
+                        numdays = CInt(lector(2).ToString)
+
+                        AgregaLista(nombre, fecha, numdays)
+                    End While
+                    lector.Close()
+
+                End Using
+                cnx.Close()
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "HA ocurrido un error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        Return Nothing
+    End Function
+
+    Private Sub TextBox2_GotFocus(sender As Object, e As System.EventArgs) Handles TextBox2.GotFocus
+        If TextBox2.Text = "Buscar por nombre" Then
+            TextBox2.ForeColor = Color.Black
+            TextBox2.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextBox2_KeyUp(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles TextBox2.KeyUp
+        recargarbusca()
+        If TextBox2.Text <> "" Then
+            Label5.Visible = True
+        Else
+            Label5.Visible = False
+        End If
+    End Sub
+
+
+    Private Sub TextBox2_LostFocus(sender As Object, e As System.EventArgs) Handles TextBox2.LostFocus
+        TextBox2.ForeColor = Color.Gray
+        TextBox2.Text = "Buscar por nombre"
+        Label5.Visible = False
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedIndex = 0 Then
+            recargarmes(1, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 1 Then
+            recargarmes(2, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 2 Then
+            recargarmes(3, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 3 Then
+            recargarmes(4, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 4 Then
+            recargarmes(5, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 5 Then
+            recargarmes(6, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 6 Then
+            recargarmes(7, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 7 Then
+            recargarmes(8, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 8 Then
+            recargarmes(9, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 9 Then
+            recargarmes(10, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 10 Then
+            recargarmes(11, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 11 Then
+            recargarmes(12, Now.Date.Year)
+        End If
+        If ComboBox1.SelectedIndex = 12 Then
+            recargar()
+        End If
+    End Sub
+
+
+    Private Sub Label5_Click(sender As System.Object, e As System.EventArgs) Handles Label5.Click
+        TextBox2.Text = ""
+        recargarbusca()
+        Label5.Visible = False
+    End Sub
+
+   
+
+End Class
